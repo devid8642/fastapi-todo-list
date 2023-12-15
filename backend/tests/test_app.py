4c5655aeeb409b3ba306107d41651901712553ee
devid <nnick4673@gmail.com>
2023-12-15 00:30:50 -0300

    Initial commit

---
+++ backend/tests/test_app.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from backend.app import app
+
+
+def test_root_returned_200_and_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Olá Mundo!'}
