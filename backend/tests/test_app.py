c43effa6e9bf141f18fddb1bd9599e204ded2e5b
Dêvid Teófilo <nnick4673@gmail.com>
2023-12-22 19:58:09 -0300

    Testing create a user with exiting email

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -17,12 +17,25 @@ def test_create_user(client):
             'password': 'secret',
         },
     )
+    fail_response = client.post(
+        '/users/create/',
+        json={
+            'username': 'devid',
+            'email': 'devid@example.com',
+            'password': 'secret',
+        },
+    )
 
     assert response.status_code == 201
     assert response.json() == {
         'id': 1,
         'username': 'devid',
         'email': 'devid@example.com',
+    }
+
+    assert fail_response.status_code == 400
+    assert fail_response.json() == {
+        'detail': 'User already registered',
     }
 
 
